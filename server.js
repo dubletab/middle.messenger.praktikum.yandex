84551c44467b354727f77fdad7f3ff1cee542401
Dmitry <gnevyshevdv@gmail.com>
2022-11-12 23:54:04 +0300

    fix crush

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
 
-app.listen(PORT, function () {
+const navArray = [
+    '/',
+    '/login',
+    '/signup',
+    '/profile',
+    '/password-change',
+    '/profile-change',
+    '/500',
+    '/incorrectName',
+];
+
+navArray.forEach((item) => {
+    app.get(item, (req, res) => {
+        res.sendFile(`${__dirname}/dist/index.html`);
+    });
+});
+
+app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
