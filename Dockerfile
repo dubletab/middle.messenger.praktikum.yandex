928251a4e2d176e8bd4c75c18fb185dca5615fcf
Dmitry <gnevyshevdv@gmail.com>
2022-11-13 00:11:54 +0300

    fix crush

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 
-CMD [ "node", "server.js"]
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(__dirname + '/dist'));
 
 const navArray = ['/', '/500', '/signup', '/messenger', '/profile', '/profile-change', '/password-change', '/*'];
 
 navArray.forEach((item) => {
     app.get(item, (req, res) => {
-        res.sendFile(`${__dirname}/dist/index.html`);
+        res.sendFile(__dirname + '/dist/index.html');
     });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
