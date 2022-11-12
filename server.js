fc6e64e28ef6075df977146ec1a9f172f20f274a
Dmitry <gnevyshevdv@gmail.com>
2022-11-12 21:19:45 +0300

    add dockerfile + precommit

--- server.js
+++ server.js
@@ -5,23 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist'));
 
-const navArray = [
-    '/',
-    '/login',
-    '/signup',
-    '/profile',
-    '/password-change',
-    '/profile-change',
-    '/500',
-    '/incorrectName',
-];
-
-navArray.forEach((item) => {
-    app.get(item, (req, res) => {
-        res.sendFile(__dirname + '/dist/index.html');
-    });
-});
-
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
